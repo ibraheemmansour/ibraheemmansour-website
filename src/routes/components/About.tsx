19f3c0d2915a0bd792740f7bade8f42222afc200
Ibraheem Mansour <ibraheem.mansour@telus.com>
2024-05-23 03:22:49 +0300

    working on about

--- src/routes/components/About.tsx
+++ src/routes/components/About.tsx
@@ -1,20 +1,15 @@
 const AboutSection = () => (
     <section class="resume-section" id="about">
-       <div class="row resume-section-content">
-            <div class="col-lg-5">
-                <img src="assets/img/main_photo.png" class="animated-image" alt="Animated Image" />
-            </div>
-            <div class="col-lg-7">
-                <h2 class="mb-0">
-                    About <span class="text-primary">Me</span>
-                </h2>
-                <h4 class="mb-5">
-                    Senior Software Developer
-                </h4>
-                <p class="lead">
-                    I hold a Master's degree in Computer and Communications Engineering and have over seven years of experience in full-stack software development tailored to client needs. I am single and, in my free time, I enjoy writing, reading, watching films, and hiking.
-                </p>
-            </div>
+       <div class="resume-section-content justify-content-around align-items-center">
+            <h2 class="mb-0">
+                About <span class="text-primary">Me</span>
+            </h2>
+            <h4 class="mb-5">
+                Senior Software Developer
+            </h4>
+            <p class="lead text-justify">
+                I hold a Master's degree in Computer and Communications Engineering and have over seven years of experience in full-stack software development tailored to client needs. I am single and, in my free time, I enjoy writing, reading, watching films, and hiking.
+            </p>
         </div>
     </section>
 );
